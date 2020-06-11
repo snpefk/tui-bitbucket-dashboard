1b8174847a57a83863cf456349a86cd3aed652a4
snpefk <snpefk@gmail.com>
2020-06-11 15:35:32 +0300

    Add to bitbucket constructor host & project parameter

--- src/bitbucket.rs
+++ src/bitbucket.rs
@@ -2,15 +2,25 @@ use data_encoding::BASE64;
 
 pub struct BitBucket<'a> {
     pub auth_header: (&'a str, String),
+    pub project_url: String,
 }
 
 impl<'a> BitBucket<'a> {
-    pub fn new(user: &str, password: &str) -> Self {
+    const PR_PATH: &'a str = "/{repo}/pull-requests";
+
+    pub fn new(user: &str, password: &str, host: &str, project: &str) -> Self {
         let credentials = format!("{}:{}", user, password);
         let base64 = BASE64.encode(credentials.as_bytes());
 
+        let project_url = format!(
+            "https://{host}/rest/api/1.0/projects/{project}/repos",
+            host = host,
+            project = project
+        );
+
         Self {
             auth_header: ("Authorization", base64),
+            project_url,
         }
     }
 }
