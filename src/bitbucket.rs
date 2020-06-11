f24d0c69f0f55d942feba9acd960e572fa1700bb
snpefk <snpefk@gmail.com>
2020-06-11 15:25:10 +0300

    fix bitbucket wrapper visibility

--- src/bitbucket.rs
+++ src/bitbucket.rs
@@ -1,9 +1,11 @@
-struct BitBucket<'a> {
-    auth_header: (&'a str, String),
+use data_encoding::BASE64;
+
+pub struct BitBucket<'a> {
+    pub auth_header: (&'a str, String),
 }
 
 impl<'a> BitBucket<'a> {
-    fn new(user: &str, password: &str) -> Self {
+    pub fn new(user: &str, password: &str) -> Self {
         let credentials = format!("{}:{}", user, password);
         let base64 = BASE64.encode(credentials.as_bytes());
 
